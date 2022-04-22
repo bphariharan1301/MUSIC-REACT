aa73acefb44b99860a6b124383ee6713d68a0b7d
HARIHARAN B P <bphariharan1301@gmail.com>
2022-04-22 15:48:08 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import { View, StatusBar, StyleSheet } from 'react-native'
-import React from 'react'
-import MusicPlayer from './screens/MusicPlayer'
+import { View, StatusBar, StyleSheet, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import auth from '@react-native-firebase/auth';
 
+import Navigation from './Navigation';
 
 const App = () => {
+
+
+  // function LoginApp() {
+  //   // Set an initializing state while Firebase connects
+  //   const [initializing, setInitializing] = useState(true);
+  //   const [user, setUser] = useState();
+  
+  //   // Handle user state changes
+  //   function onAuthStateChanged(user) {
+  //     setUser(user);
+  //     if (initializing) setInitializing(false);
+  //   }
+  
+  //   useEffect(() => {
+  //     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  //     return subscriber; // unsubscribe on unmount
+  //   }, []);
+  
+  //   if (initializing) return null;
+  
+  //   if (!user) {
+  //     return (
+  //       <View>
+  //         <Text>Login</Text>
+  //       </View>
+  //     );
+  //   }
+  
+  //   return (
+  //     <View>
+  //       <Text>Welcome {user.email}</Text>
+  //     </View>
+  //   );
+  // }
+
   return (
     <View style={style.cotainer}>
       <StatusBar barStyle='light-content' />
-      <MusicPlayer />
+
+      <Navigation />
+
     </View>
   )
 }
